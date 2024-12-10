79b205991af26e470cbdcb1766e8275896d2a9dc
msuez <matisuez@gmail.com>
2024-12-09 21:09:32 -0300

    First commit

---
+++ src/config/envs.ts
@@ -0,0 +1,29 @@
+import 'dotenv/config';
+import * as joi from 'joi';
+
+interface EnvVars {
+    PORT: number;
+    DATABASE_URL;
+}
+
+const envsSchema = joi.object({
+    PORT: joi.number().required(),
+    DATABASE_URL: joi.string().required(),
+}).unknown(true);
+
+const {
+    error, value,
+} = envsSchema.validate( process.env );
+
+if ( error ) {
+    throw new Error(`Config validation errors: ${ error.message }`);
+}
+
+const envVars: EnvVars = value;
+
+export const envs = {
+    port: envVars.PORT,
+    databaseUrl: envVars.DATABASE_URL,
+}
+
+
---
+++ src/products/products.service.ts
@@ -0,0 +1,98 @@
+import { PrismaClient } from '@prisma/client';
+import { Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
+
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PaginationDto } from 'src/common';
+
+@Injectable()
+export class ProductsService extends PrismaClient implements OnModuleInit {
+
+  private readonly logger = new Logger('ProductService');
+
+  onModuleInit() {
+    this.$connect();
+    this.logger.log(`Database connected!`);
+  }
+
+  async create(createProductDto: CreateProductDto) {
+    const product = await this.product.create({
+      data: createProductDto,
+    });
+    return product;
+  }
+
+  async findAll( paginationDto: PaginationDto ) {
+
+    const { page, limit, } = paginationDto;
+    const totalPages = await this.product.count({
+      where: {
+        available: true,
+      },
+    });
+    const lastPage = Math.ceil( totalPages / limit );
+
+    const meta = { page, lastPage, totalPages, };
+
+    const data = await this.product.findMany({
+      where: {
+        available: true,
+      },
+      take: limit,
+      skip: ( page - 1 ) * limit,
+    });
+
+    return {
+      meta,
+      data,
+    };
+
+  }
+
+  async findOne(id: number) {
+    const product = await this.product.findFirst({
+      where: {
+        id,
+        available: true,
+      },
+    });
+
+    if ( !product ) {
+      throw new NotFoundException(
+        `Product with id ${ id } not found.`,
+      );
+    }
+
+    return product;
+  }
+
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    const {
+      id: __, 
+      ...data
+    } = updateProductDto;
+    await this.findOne( id );
+    const product = await this.product.update({
+      where: {
+        id,
+        available: true,
+      },
+      data,
+    });
+    return product;
+  }
+
+  async remove(id: number) {
+    await this.findOne( id );
+    // const product = await this.product.delete({
+    //   where: { id, },
+    // });
+    const product = await this.product.update({
+      where: { id, },
+      data: {
+        available: false,
+      }
+    });
+    return product;
+  }
+}
